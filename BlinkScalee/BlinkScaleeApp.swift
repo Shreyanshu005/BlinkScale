4188e71e74644e006555c5c47c3b091480774315
Shreyanshu005 <shreyanshu005@gmail.com>
2026-07-18 17:13:16 +0530

    Initial Commit

---
+++ BlinkScalee/BlinkScaleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkScaleeApp.swift
+//  BlinkScalee
+//
+//  Created by Shreyanshu on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkScaleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlinkScalee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlinkScalee
+//
+//  Created by Shreyanshu on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
